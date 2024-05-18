b5149c57ac202b73f0cf0f77143464280badf6e6
LegGodtPruebas <javiersalinas69x100@gmail.com>
2024-05-17 22:49:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function testimonialClick(){
+    alert("Sorry, we haven't worked on editing this box :( \nWe hope to update this soon!");
+}
+
+function openMenu (){
+    mainMenu.classList.toggle('main-menu--show');
+    toggleMenu.classList.toggle('movement-menu--toggle');
+    main.classList.toggle('movement-container--toggle');
+    body.classList.toggle('movement-body-height--togle');
+}
